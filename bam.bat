7444697eb6cc62e0497092fc1bdc4e4f5b4a7ca3
GreYFoX <greyfoxgti@gmail.com>
2011-12-14 16:21:30 +0200

    Some files that i always need to install

---
+++ bam.bat
@@ -0,0 +1,31 @@
+@echo off
+
+@REM check if we already have the tools in the environment
+if exist "%VCINSTALLDIR%" (
+	goto compile
+)
+
+@REM Check for Visual Studio
+if exist "%VS100COMNTOOLS%" (
+	set VSPATH="%VS100COMNTOOLS%"
+	goto set_env
+)
+if exist "%VS90COMNTOOLS%" (
+	set VSPATH="%VS90COMNTOOLS%"
+	goto set_env
+)
+if exist "%VS80COMNTOOLS%" (
+	set VSPATH="%VS80COMNTOOLS%"
+	goto set_env
+)
+
+echo You need Microsoft Visual Studio 8, 9 or 10 installed
+pause
+exit
+
+@ setup the environment
+:set_env
+call %VSPATH%vsvars32.bat
+
+:compile
+%appdata%\bam.exe %*
---
+++ win32_install.bat
@@ -0,0 +1,3 @@
+copy /Y bam.exe %appdata%\bam.exe
+copy /Y bam.bat %windir%\bam.bat
+pause
